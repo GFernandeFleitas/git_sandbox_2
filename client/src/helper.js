0c22013929afc2de9a7796a684640b4d9066329c
GFernandeFleitas <derlis.fernandez15@gmail.com>
2024-03-22 12:46:43 -0300

    another comment

--- client/src/helper.js
+++ client/src/helper.js
@@ -2,4 +2,6 @@ print("aqui esta la prueba");
 
 //!THIS IS STILL PYTHON BRO
 
-// is js not py what are you seeing ?? 
+// is js not py what are you seeing ??
+
+//The thing is that the file extension 'js' does not match the python file '.py' haha but the code is python
