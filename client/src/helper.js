5afd3953c8762b8da32ca14544a80d9a3879d252
Daniel <danielmasmar@gmail.com>
2024-03-23 17:41:12 -0400

    commit

--- client/src/helper.js
+++ client/src/helper.js
@@ -5,3 +5,8 @@ print("aqui esta la prueba");
 // is js not py what are you seeing ??
 
 //The thing is that the file extension 'js' does not match the python file '.py' haha but the code is python
+
+
+// hola que tal
+
+// otra vez
